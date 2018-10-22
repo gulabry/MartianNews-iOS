322fb449f59b6ecadbd438d843accb9dd8d3dc08
Bryan Gula <bryan@gula.io>
2018-10-22 16:56:07 -0700

    Initial Commit

---
+++ MartianNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MartianNews
+//
+//  Created by Bryan Gula on 10/22/18.
+//  Copyright © 2018 Gula, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
